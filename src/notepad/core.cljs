1c2a4681d96f6c51894914c59ee2d5141278ed06
Wilkes Joiner <wilkesjoiner@gmail.com>
2011-08-10 00:51:44 -0500

    part 2 finished

--- src/notepad/core.cljs
+++ src/notepad/core.cljs
@@ -1,13 +1,16 @@
 (ns notepad.core
   (:require [goog.dom :as dom]
-            [goog.ui.Zippy :as zippy]))
+            [goog.ui.Zippy :as zippy]
+            [goog.events :as events]
+            [goog.events.EventType :as event-type]))
 
 (defn make-note-dom [parent {:keys [title content]}]
-  (let [header-element (dom/createDom "div"
+  (let [content (atom content)
+        header-element (dom/createDom "div"
                                       (.strobj {"style"
                                                 "background-color:#EEE"})
                                       title)
-        content-element (dom/createDom "div" nil content)
+        content-element (dom/createDom "div" nil @content)
         editor-element (dom/createDom "textarea")
         save-button (dom/createDom "input" (.strobj {"type" "button"
                                                      "value" "Save"}))
@@ -19,6 +22,17 @@
                                          content-element editor-container)
         new-note (dom/createDom "div" nil header-element content-container)]
     (dom/appendChild parent new-note)
+    (events/listen content-element event-type/CLICK
+                   (fn [e]
+                     (set! (.. editor-element value) @content)
+                     (set! (.. content-element style display) "none")
+                     (set! (.. editor-container style display) "inline")))
+    (events/listen save-button event-type/CLICK
+                   (fn [e]
+                     (reset! content (.value editor-element))
+                     (set! (.. content-element innerHTML) @content)
+                     (set! (.. content-element style display) "inline")
+                     (set! (.. editor-container style display) "none")))
     (goog.ui.Zippy. header-element content-element)))
 
 (defn make-notes [data note-container]
