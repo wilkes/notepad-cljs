0471f68d0288cd23e75c343c623d4e31d68f2552
Wilkes Joiner <wilkesjoiner@gmail.com>
2011-08-13 09:59:03 -0500

    made note more objecty

--- src/notepad/core.cljs
+++ src/notepad/core.cljs
@@ -10,13 +10,12 @@
 (defn hide [el]
   (set! (.. el style display) "none"))
 
-(defn make-note-dom [parent {:keys [title content]}]
-  (let [content (atom content)
-        header-element (dom/createDom "div"
+(defn create-note [title content]
+  (let [header-element (dom/createDom "div"
                                       (.strobj {"style"
                                                 "background-color:#EEE"})
                                       title)
-        content-element (dom/createDom "div" nil @content)
+        content-element (dom/createDom "div" nil content)
         editor-element (dom/createDom "textarea")
         save-button (dom/createDom "input" (.strobj {"type" "button"
                                                      "value" "Save"}))
@@ -24,24 +23,41 @@
                                         (.strobj {"style" "display:none"})
                                         editor-element
                                         save-button)
+
         content-container (dom/createDom "div" nil
                                          content-element editor-container)
-        new-note (dom/createDom "div" nil header-element content-container)]
-    (dom/appendChild parent new-note)
-    (events/listen content-element event-type/CLICK
-                   (fn [e]
-                     (dom/setTextContent editor-element @content)
-                     (hide content-element)
-                     (show editor-container)))
-    (events/listen save-button event-type/CLICK
-                   (fn [e]
-                     (reset! content (.value editor-element))
-                     (dom/setTextContent content-element @content)
-                     (show content-element)
-                     (hide editor-container)))
-    (goog.ui.Zippy. header-element content-element)))
+        container (dom/createDom "div" nil header-element content-container)]
+    (atom {:content content
+           :header-element header-element
+           :content-element content-element
+           :save-button save-button
+           :editor-element editor-element
+           :editor-container editor-container
+           :content-container content-container
+           :container container})))
+
+(defn edit-note [note]
+  (fn [e]
+    (dom/setTextContent (:editor-element @note) (:content @note))
+    (hide (:content-element @note))
+    (show (:editor-container @note))))
+
+(defn save-note [note]
+   (fn [e]
+     (swap! note assoc :content (.value (:editor-element @note)))
+     (dom/setTextContent (:content-element @note) (:content @note))
+     (show (:content-element @note))
+     (hide (:editor-container @note))))
+
+(defn make-note-dom [parent {:keys [title content]}]
+  (let [note (create-note title content)]
+    (dom/appendChild parent (:container @note))
+    (events/listen (:content-element @note) event-type/CLICK (edit-note note))
+    (events/listen (:save-button @note) event-type/CLICK (save-note note))
+    (goog.ui.Zippy. (:header-element @note) (:content-element @note))))
 
 (defn make-notes [data note-container]
+  (dom/appendChild note-container (dom/createDom "h3" nil "Notes"))
   (doseq [d data]
     (make-note-dom note-container d)))
 
@@ -50,6 +66,6 @@
                  :content "Content of Note 1"}
                 {:title "Note 2"
                  :content "Content of Note 2"}]
-              (dom/$ "notes")))
+              (dom/getElement "notes")))
 
 (main)
